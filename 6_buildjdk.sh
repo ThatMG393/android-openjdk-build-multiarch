76b73ff9887c4cfa3521a4f50cf4f71eaee1c30d
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2024-10-31 17:10:09 +0800

    `-O3` -> `-Ofast`

--- 6_buildjdk.sh
+++ 6_buildjdk.sh
@@ -7,7 +7,7 @@ export CUPS_DIR=$PWD/cups-2.2.4
 export CFLAGS+=" -DLE_STANDALONE" # -I$FREETYPE_DIR -I$CUPS_DI
 if [[ "$TARGET_JDK" == "arm" ]] # || [[ "$BUILD_IOS" == "1" ]]
 then
-  export CFLAGS+=" -O3 -D__thumb__"
+  export CFLAGS+=" -Ofast -D__thumb__"
 else
   if [[ "$TARGET_JDK" == "x86" ]]; then
      export CFLAGS+=" -O3 -mstackrealign"
